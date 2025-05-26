441220029afe451f9a902583d838d6a4168e79ce
mvghasty <fasmagoric.dev@gmail.com>
2025-05-26 18:26:15 -0300

    Initial commit

---
+++ lib/rbforge.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbforge/version"
+
+module Rbforge
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbforge/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rbforge
+  VERSION = "0.1.0"
+end
