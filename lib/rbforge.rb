f3bc96b17deab1b5104f7641f3177f6ecf32947d
mvghasty <fasmagoric.dev@gmail.com>
2025-05-26 18:29:50 -0300

    Initial Commit - Adding modifications

--- lib/rbforge.rb
+++ lib/rbforge.rb
@@ -1,8 +1,38 @@
 # frozen_string_literal: true
 
 require_relative "rbforge/version"
+require 'fileutils'
 
 module Rbforge
   class Error < StandardError; end
-  # Your code goes here...
+  class CLI
+    def self.start(args)
+      case args[0]
+      when 'init'
+        init_c
+      else
+        puts "Comando desconhecido. Use `init`."
+      end
+    end
+
+    def self.init_c
+      puts "[*] - FORGING your ruby project..."
+
+      FileUtils.mkdir_p('lib')
+      FileUtils.touch('Gemfile')
+      FileUtils.touch('README.md')
+      FileUtils.touch('.gitignore')
+      FileUtils.touch('lib/main.rb')
+
+      File.open('Gemfile', "w") do |g_file|
+        g_file.puts 'source "https://rubygems.org"'
+      end
+
+      File.open('lib/main.rb', "w") do |m_file|
+        m_file.puts 'puts "Hello, world"'
+      end
+
+      puts "[*] - Done!"
+    end
+  end
 end
